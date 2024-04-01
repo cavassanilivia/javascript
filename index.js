bbec9e02186cad80ab93cc8a434b9f8e6d7d57ca
Livia Cavassani Galdino <cavassanilivia@gmail.com>
2024-04-01 10:25:29 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<heade>
+    <meta charset="UTF-8">
+    <title>Javascript Hello Word!</title>
+
+    <script>
+        function main() {
+            alert("Hello Word!"):
+        }
+        </script>
+</heade>
+<body>
+    
+</body>
+</html>
+            
+        
